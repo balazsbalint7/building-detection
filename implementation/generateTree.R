f498775fde7058c1dd25689e3d99f9639c398a77
Balint Balazs <balint.balazs@aimotive.com>
2023-05-02 03:40:21 +0200

    fixing r file

--- implementation/generateTree.R
+++ implementation/generateTree.R
@@ -1,5 +1,6 @@
 install.packages("lidR")
 library(lidR)
+library(terra)
 
 LASfile <- system.file("extdata", "/lidar_data/20011104_950.laz", package="lidR")
 las <- readLAS(LASfile, select = "xyzr", filter = "-drop_z_below 0")
@@ -14,3 +15,5 @@ ttops_chm_p2r_05_smoothed <- locate_trees(chm_p2r_05_smoothed, lmf(5))
 
 algo <- dalponte2016(chm_p2r_05_smoothed, ttops_chm_p2r_05_smoothed)
 las <- segment_trees(las, algo) # segment point cloud
+
+writeLas(las, "/lidar_data/20011104_950_trees.laz")
