1c03507198878950cb7b4eb4c0184333d06f198a
Balint Balazs <balint.balazs@aimotive.com>
2023-05-02 03:31:41 +0200

    added r file to generate trees

---
+++ implementation/generateTree.R
@@ -0,0 +1,16 @@
+install.packages("lidR")
+library(lidR)
+
+LASfile <- system.file("extdata", "/lidar_data/20011104_950.laz", package="lidR")
+las <- readLAS(LASfile, select = "xyzr", filter = "-drop_z_below 0")
+
+# Point-to-raster 2 resolutions
+chm_p2r_05 <- rasterize_canopy(las, 0.5, p2r(subcircle = 0.2), pkg = "terra")
+
+# Post-processing median filter
+kernel <- matrix(1,3,3)
+chm_p2r_05_smoothed <- terra::focal(chm_p2r_05, w = kernel, fun = median, na.rm = TRUE)
+ttops_chm_p2r_05_smoothed <- locate_trees(chm_p2r_05_smoothed, lmf(5))
+
+algo <- dalponte2016(chm_p2r_05_smoothed, ttops_chm_p2r_05_smoothed)
+las <- segment_trees(las, algo) # segment point cloud
