7282d56bb843f8a99b9b872fb9cc4a0f28ec3fb8
Balázs Bálint <balazs.balint413@gmail.com>
2023-05-23 01:54:48 +0200

    slides added, final changes

--- implementation/generateTree.R
+++ implementation/generateTree.R
@@ -2,18 +2,20 @@ install.packages("lidR")
 library(lidR)
 library(terra)
 
-LASfile <- system.file("extdata", "/lidar_data/20011104_950.laz", package="lidR")
-las <- readLAS(LASfile, select = "xyzr", filter = "-drop_z_below 0")
+# set 
+las <- readLAS("lidar_data/20011104_950.las", select = "xyzr", filter = "-drop_z_below 0")
 
 # Point-to-raster 2 resolutions
-chm_p2r_05 <- rasterize_canopy(las, 0.5, p2r(subcircle = 0.2), pkg = "terra")
+# chm_p2r_05 <- rasterize_canopy(las, 0.5, p2r(subcircle = 0.2), pkg = "terra")
 
-# Post-processing median filter
-kernel <- matrix(1,3,3)
-chm_p2r_05_smoothed <- terra::focal(chm_p2r_05, w = kernel, fun = median, na.rm = TRUE)
-ttops_chm_p2r_05_smoothed <- locate_trees(chm_p2r_05_smoothed, lmf(5))
+# Post-processing median filter (not neccessary)
+# kernel <- matrix(1,3,3)
+# chm_p2r_05_smoothed <- terra::focal(chm_p2r_05, w = kernel, fun = median, na.rm = TRUE)
+# ttops_chm_p2r_05_smoothed <- locate_trees(chm_p2r_05_smoothed, lmf(5))
 
-algo <- dalponte2016(chm_p2r_05_smoothed, ttops_chm_p2r_05_smoothed)
-las <- segment_trees(las, algo) # segment point cloud
+# algo <- dalponte2016(chm_p2r_05_smoothed, ttops_chm_p2r_05_smoothed)
+# las <- segment_trees(las, algo) # segment point cloud
 
-writeLas(las, "/lidar_data/20011104_950_trees.laz")
+las <- segment_trees(las, li2012(R = 3, speed_up = 5))
+
+writeLas(las, "/lidar_data/20011104_950_trees.las")
